df31b74e5f7cfb420112953cf9068c91966bbf18
muhammadyamanmamji <143739066+muhammadyamanmamji@users.noreply.github.com>
2023-09-25 05:40:57 -0700

    Add files via upload

---
+++ 20 to 30/app.js
@@ -0,0 +1,153 @@
+
+// ______________________-------------chapter 21-----------------_______________________________
+
+
+// var userinput = userinput.toLocaleLowerCase();
+
+
+// var x =("MY NAME IS YAMAN")
+
+// var x = x.toLocaleLowerCase();
+// console.log(x)
+
+
+// var y =("my name is yaman")
+
+// var y = y.toLocaleUpperCase();
+// console.log(y)
+
+
+// input_string = "Hello World"
+// lowercase_string = input_string.toLowerCase()
+// console.log(lowercase_string)
+  
+// ##########################################????????? QS no 5 unsolve
+// debugger
+// var input_str = ["Helloe World scsecsdHsdc"]
+// input_str =  input_str.toLocaleLowerCase()
+// console.log(input_str)
+
+// var str = ["hello world"];
+// str = str.toUpperCase();
+// console.log(str);
+
+// a ="my name is yaman "
+// Uppercase_string = a.tolocalUpperCase()
+// alert(Uppercase_string)
+
+
+//  var cityName = "kaRacHi";
+ 
+// var length =  cityName.slice(0,1);
+// var length_2 = cityName.slice(1,7)
+// length=length.toUpperCase()
+// length_2=length_2.toLowerCase()
+// console.log(length +=length_2)
+
+// var word ="captain";
+//  word=word.slice(1,3)
+// console.log(word)
+
+// var num =",2,3 ,5 ,7 , 9,"
+
+// var animal = "elephent"
+// var animal_2 = animal.slice(2,6);
+// console.log(animal_2)
+
+
+// var lorem_words ="this is lorem words"
+// var lorem_words2 =lorem_words.length();
+// console.log(lorem_words2)
+
+
+// var word = "Hello World"
+// var length = word.length
+// var first = word.slice (0,1)
+// var last = word.slice (8,11)
+
+// console.log(length + " " + first +last);
+
+// var text = "To be or not to be.";
+// var indx = text.indexOf("be");
+// console.log(indx)
+
+
+// var text = "To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// console.log(indx)
+
+// var str = "hello world"
+// var num = 6;
+
+// if (str.charAt(num) !== undefined) {
+//     console.log("Character at index " + num + " exists: " + str.charAt(num));
+//   } else {
+//     console.log("Character at index " + num + " does not exist.");
+//   }
+
+//   if (str[num] !== undefined) {
+//     console.log("Character at index " + num + " exists: " + str[num]);
+//   } else {
+//     console.log("Character at index " + num + " does not exist.");
+// }
+
+// var word = "abcde"
+// var index = word.charAt(1)
+// console.log(index)
+
+// var char = "variable cha,"
+
+// var char1 = char.charAt(10)
+// console.log(char1);
+
+// var indexNo = "yaman"
+
+// var lastIndex = indexNo.lastIndexOf("n")
+// lastIndex = indexNo.replace("n","x")
+// console.log(lastIndex)
+
+
+
+
+// var num =3.2 ;
+// console.log(Math.round(num));
+
+
+
+// var num =3.1 ;
+// console.log(Math.floor(num));
+
+
+// var num =3.004 ;
+// console.log(Math.ceil(num));
+
+
+// var num =0.5 ;
+// console.log(Math.floor(num));
+
+
+
+// var random = Math.random()*50
+// console.log(random.toFixed(0));
+
+
+// var RandomNum = Math.random()
+// console.log(RandomNum)
+// debugger
+// var headUser = prompt("Enter head Username")
+// var tailUser = prompt("Enter tail Username")
+
+// var toss = Math.random()*2
+// var floor = Math.floor(toss)
+// alert(floor)
+
+// if(floor === 0){
+//     alert(headUser + "win the toss")
+// }else{
+//     alert(tailUser + "win the toss")
+// }
+
+var str = "yaman"
+str = Number.parseInt(str)
+str = typeof(str)
+console.log(str) 
